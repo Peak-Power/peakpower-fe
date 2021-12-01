62b37bd49409c18c25fab1ae91c0b8156d015958
Luis del Rio Francos <ldelriof@gmail.com>
2021-12-01 14:21:48 +0100

    remove funding, change wording

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import logo from '../public/imgs/logo.png'
-import container from '../public/imgs/container.png'
-import balance from '../public/imgs/balance.png'
 import storage from '../public/imgs/storage.png'
-import secondlife from '../public/imgs/secondlife.png'
+import secondlife from '../public/imgs/secondlife3.png'
 
 
 export default function Home() {
@@ -23,16 +21,8 @@ export default function Home() {
                 src={logo}
           />
           <p className={styles.description}>
-            THE DISTRIBUTED<br />BALANCED<br />GRID
+            THE DISTRIBUTED<br />ENERGY<br />BALANCER
           </p>
-        </div>
-        <div className={styles.elev}>
-          <h2><span className={styles.red}>INVEST</span> IN THE ENERGY TRANSITION</h2>
-          <p>Get a <span className={styles.red}>5%</span> anual return + Market Based Premium</p>
-          <p>Run-time	Up to <span className={styles.red}>8</span> years</p>
-          <p>Start from €1.000</p>
-
-          <input placeholder="email@example.com"></input> <button>Join to our waiting list</button>
         </div>
       </div>
 
@@ -49,8 +39,12 @@ export default function Home() {
 
             <div className={styles.col, styles.text}>
               <p>
+                For grid providers it is costly and challenging to maintain a <span className={styles.red}>balanced grid</span>. 
+                especially with the energy transition advancing.
+              </p>
+              <p>
                 Renewable Energy is an <span className={styles.red}>intermitent</span> source of energy 
-                that needs to be <span className={styles.red}>stored</span> to be able to replace carbon based 
+                that needs to be <span className={styles.red}>stored</span> to be able to fully replace carbon based 
                 energy plants 
               </p>
             </div>
@@ -79,55 +73,20 @@ export default function Home() {
             </div>
             <div className={styles.col, styles.text}>
               <p>
-                Electric Vehicles (EVs) are key player on the clean energy transition
+                Electric Vehicles (EVs) are <span className={styles.red}> key player on the clean energy transition </span>
                 but they also represent a danger for the environment if we
-                don&lsquo;t take care of the waste after their useful life.
+                don&lsquo;t take care of the <span className={styles.red}>waste</span> after their useful life.
               </p>
               <p>
-                We give <span className={styles.red}>obsolete EVs batteries</span> a second purpose. 
-                By assembling them together and
-                connecting them to the grid we <span className={styles.red}>contribute to balancing </span> 
-                the network by 
-                taking energy from the grid when the sun shines and there&lsquo;s no one using 
+                Rapid <span className={styles.red}>advances on battery technologies</span> make old EV batteries obsolete 
+                before they are fully used, we give this batteries a second purpose by
+                <span className={styles.red}> contributing to balance </span> 
+                the grid charging the batteries when the sun shines and there&lsquo;s no one using 
                 that power, then later we can feed that energy back in when needed.
               </p>
             </div>
           </div>
         </div>
-
-
-
-        <div className={styles.blackStripe}>
-          <h2>
-          BECOME ONE OF THE <span className={styles.red}>DISTRIBUTED</span> OWNERS
-          </h2>
-        </div>
-
-        <div className={styles.whiteStripe}>
-          <div className={styles.product}>
-            <div className={styles.col}>
-            <Image 
-                  src={container}
-            />
-            </div>
-            <div className={styles.col}>
-              <p>
-                To make this transition possible we need you. 
-                Our <span className={styles.red}>2MW </span> 
-                batteries will be tokenized enabling anyone to take part 
-                of the transition.
-              </p>
-              <p>Get a <span className={styles.red}>5%</span> anual return + Market Based Premium</p>
-              <p>Run-time	Up to <span className={styles.red}>8</span> years</p>
-              <p>Start from €1.000</p>
-
-              <input placeholder="email@example.com"></input> <button>Join to our waiting list</button>
-            </div>
-          </div>
-        </div>
-
-
-
 
         <div className={styles.blackStripe, styles.footer}>
           Questions? Write us: &nbsp;<a href="mailto:info@peakpower.io">info@peakpower.io</a>
